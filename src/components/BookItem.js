e5c7bf5ee6c3e4ffe6ac0296950a37abe74e8ab7
olipliche <olipliche@gmail.com>
2021-10-09 16:03:58 +0100

    Update BookItem file

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -3,23 +3,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const BookItem = ({ book, removeBookFromStoreProps }) => (
-  <li key={book.item_id}>
-    <p>
-      {book.title}
-      <span>
-      &nbsp;in&nbsp;
-        {book.category}
-        &nbsp;category
-      </span>
-    </p>
-    <button
-      type="button"
-      onClick={() => {
-        removeBookFromStoreProps(book.item_id);
-      }}
-    >
-      Remove
-    </button>
+  <li className="book-item" key={book.item_id}>
+    <div className="book-item__left">
+      <p className="book-item__p--category">{book.category}</p>
+      <p className="book-item__p--title">{book.title}</p>
+      <p className="book-item__p--author">Book Author</p>
+      <div className="book-item__btn-group">
+        <button className="book-item__btn" type="button">
+          Comments
+        </button>
+        <div className="v-divider" />
+        <button
+          className="book-item__btn"
+          type="button"
+          onClick={() => {
+            removeBookFromStoreProps(book.item_id);
+          }}
+        >
+          Remove
+        </button>
+        <div className="v-divider" />
+        <button className="book-item__btn" type="button">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div className="book-item__right">
+      <div className="book-item__progress">
+        <div className="book-item__progress-circle" />
+        <div className="book-item__progress-text">
+          <p className="book-item__progress-text--percent">64%</p>
+          <p className="book-item__progress-text--status">Completed</p>
+        </div>
+      </div>
+      <div className="v-divider" />
+      <div className="book-item__chapter">
+        <p className="book-item__chapter-title">CURRENT CHAPTER</p>
+        <p className="book-item__chapter-number">
+          Chapter 17: &ldquo;The Silent One&rdquo;
+        </p>
+        <button className="book-item__chapter-btn" type="button">
+          UPDATE PROGRESS
+        </button>
+      </div>
+    </div>
   </li>
 );
 
